53115765612658d296e4bc2053be63385454a199
parvezahmedweb1 <parvezahmedweb1@gmail.com>
2022-10-26 17:44:56 +0600

    get categories data on server

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
-
+// const cars = require("cors");
 const port = process.env.PORT || 5000;
-
+// app.use(cars);
+// ? data
+const categories = require("./data/categories.json");
 app.get("/", (req, res) => {
   res.send("iAcademy Server in Running...");
+});
+// ? categories
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
