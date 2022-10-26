ca08fa4eeb407198943c42ee21883318d3e56743
parvezahmedweb1 <parvezahmedweb1@gmail.com>
2022-10-26 23:56:37 +0600

    get courses data on server

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const app = express();
-// const cars = require("cors");
 const port = process.env.PORT || 5000;
-// app.use(cars);
+const cors = require("cors");
+app.use(cors());
 // ? data
 const categories = require("./data/categories.json");
+const courses = require("./data/courses.json");
 app.get("/", (req, res) => {
   res.send("iAcademy Server in Running...");
 });
 // ? categories
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+// ? courses
+app.get("/courses", (req, res) => {
+  res.send(courses);
 });
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
