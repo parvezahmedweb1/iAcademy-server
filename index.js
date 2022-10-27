915115d9f3af595aad924be402c102e0c3b1a60e
parvezahmedweb1 <parvezahmedweb1@gmail.com>
2022-10-27 10:28:09 +0600

    get the courses detailt

--- index.js
+++ index.js
@@ -13,12 +13,8 @@ app.get("/", (req, res) => {
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
-// ? courses
-app.get("/courses", (req, res) => {
-  res.send(courses);
-});
 // ? categories by id
-app.get("/courses/:id", (req, res) => {
+app.get("/categories/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const coursesById = courses.filter((course) => course.categoryId === id);
   res.send(coursesById);
@@ -26,6 +22,12 @@ app.get("/courses/:id", (req, res) => {
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
+app.get("/courses/:id", (req, res) => {
+  const coursesId = req.params.id;
+  const courser = courses.find((course) => course._id === coursesId);
+  res.send(courser);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
